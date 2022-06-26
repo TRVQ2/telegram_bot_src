2576af2a9c09012ddf09dfe2f357b975f1e4696c
Ruslan Tomashevsky <Ruslan.Tomashevsky@gmail.com>
2022-06-26 23:54:09 +0300

    Initial version of Telegram bot. 1) bot.py - handles connection with Telegram server and messaging 2) The communication flow can be tested via "commands.py" 3) /lyrics command supported. Tries to find lyrics for the song from the supported sites

---
+++ bot.py
@@ -0,0 +1,26 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from commands import *
+
+import os
+
+
+bot = Bot(token=os.getenv('TOKEN'))
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def echo_send(message: types.Message):
+    text = process_message(message.text)
+    if text is None or len(text) == 0:
+        return
+    print("answer...")
+    await message.answer(text)
+    # await message.answer(message.text)  # answer
+    # await message.reply(message.text)  # reply to message
+    # await bot.send_message(message.from_user.id, message.text)  # to private
+
+
+# skip_updates=true - to check only those messages when bot is online
+executor.start_polling(dp, skip_updates=True)
---
+++ commands.py
@@ -0,0 +1,144 @@
+import requests
+from lxml import etree
+import lxml.html
+import re
+
+
+def parse_page(url):
+    try:
+        r = requests.get(url)
+        if r.status_code != 200:
+            return
+    except requests.exceptions.RequestException as e:
+        return
+
+    return lxml.html.document_fromstring(r.text)
+
+
+# https://lyrsense.com/blackmores_night/wind_in_the_willows ===================
+def lyrsense_prepare_url(band, song):
+    if band is None or len(band) == 0 or song is None or len(song) == 0:
+        return
+    band = '_'.join(i for i in band.lower().replace("'", "").split())
+    song = '_'.join(i for i in song.lower().replace("'", "").split())
+    return f"https://lyrsense.com/{band}/{song}"
+
+
+def lyrsense_page_to_text(band, song):
+    tree = parse_page(lyrsense_prepare_url(band, song))
+    if tree is None:
+        return
+
+    lyric_ru_name = tree.xpath('//*[@id="ru_title"]/text()')
+    lyric_fr_name = tree.xpath('//*[@id="fr_title"]/text()')
+
+    return str(lyric_ru_name) + '\n\n' + \
+        ''.join(
+            '\n' if i.text is None else i.text + ' '
+            for i in tree.xpath('//*[@id="ru_text"]')[0]
+        ) + '\n=====\n' + \
+        str(lyric_fr_name) + '\n\n' + \
+        ''.join(
+            '\n' if i.text is None else i.text + ' '
+            for i in tree.xpath('//*[@id="fr_text"]')[0]
+        )
+
+
+# Musinfo specific methods ====================================================
+def musinfo_prepare_url(band, song):
+    if band is None or len(band) == 0 or song is None or len(song) == 0:
+        return
+    band = '-'.join(i for i in band.lower().split())
+    song = '-'.join(i for i in song.lower().split())
+    return f"https://ru.musinfo.net/lyrics/{band}/{song}"
+
+
+def musinfo_page_to_text(band, song):
+    tree = parse_page(musinfo_prepare_url(band, song))
+    if tree is None:
+        return
+
+    return '\n'.join(
+        '' if i.text is None else i.text
+        for i in tree.xpath('//*[@id="lyric-dst"]')[0]
+    ) + '\n=====\n' + '\n'.join(
+        '' if i.text is None else i.text
+        for i in tree.xpath('//*[@id="lyric-src"]')[0]
+    )
+
+
+def musinfo_page_to_text2(band, song):
+    tree = parse_page(musinfo_prepare_url(band, song))
+    if tree is None:
+        return None
+
+    lyric_src_name = tree.xpath('//*[@id="lyric-dst"]/div[1]/text()')
+    lyric_src = tree.xpath('//*[@id="lyric-dst"]/div[@class="line"]/text()')
+    lyric_dst_name = tree.xpath('//*[@id="lyric-src"]/div[1]/text()')
+    lyric_dst = tree.xpath('//*[@id="lyric-src"]/div[@class="line"]/text()')
+    return str(lyric_src_name) + '\n\n' + \
+        '\n'.join(i for i in lyric_src) + "\n=====\n" + \
+        str(lyric_dst_name) + '\n\n' + \
+        '\n'.join(i for i in lyric_dst)
+# Musinfo specific methods ====================================================
+
+
+def get_lyrics(path):  # expected input format 'Band|Song'
+    if path is None or len(path) < 1:
+        return supported_commands['/lyrics']['usage']
+    items = path.strip().split('|')
+    if len(items) == 1:
+        return 'Wrong request: Song is not specified. ' + \
+            supported_commands['/lyrics']['usage']
+    result = ''
+    for i in supported_commands['/lyrics']['api_list']:
+        result = i(items[0], items[1])
+        if result is not None:
+            break
+    return result
+
+
+supported_commands = {
+    '/lyrics': {
+        'desc': 'To find lyrics and translation',
+        'usage': 'Use /lyrics Band|Song',
+        'commands': [],
+        'call': get_lyrics,
+        'api_list': [lyrsense_page_to_text, musinfo_page_to_text]
+    }
+}
+
+
+def process_message(message):
+    if message is None:
+        return
+    answer = None
+    message = message.strip()
+    list = message.split(' ', 1)
+    if list[0] in supported_commands:
+        call = supported_commands[list[0]]['call']
+        answer = call(message.replace(list[0], ''))
+    elif list[0] in ('/start', '/help', '/commands'):
+        answer = '\n'.join(
+            f"{i}\t- {supported_commands[i]['desc']}" +
+            f". {supported_commands[i]['usage']}"
+            for i in supported_commands
+        )
+    return answer
+
+
+def main():
+    '''text = None
+    print(process_message(text))
+    text = ''
+    print(process_message(text))
+    text = '/lyrics'
+    print(process_message(text))
+    text = '/lyrics Metallica'
+    print(process_message(text))'''
+    text = '/lyrics Metallica|Nothing Else Matters'
+    print(process_message(text))
+
+
+if __name__ == "__main__":
+    main()
